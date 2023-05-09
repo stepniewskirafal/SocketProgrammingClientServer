75f4bbf1544bd902f5eccca33d1a3c82990b59f4
Rafał Stępniewski <vultur12@gmail.com>
2023-05-09 10:41:25 +0200

    new test class for MessageService

---
+++ src/test/java/pl/rstepniewski/sockets/domain/message/MessageServiceTest.java
@@ -0,0 +1,16 @@
+package pl.rstepniewski.sockets.domain.message;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageServiceTest {
+
+    @Test
+    void getUserMessages() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+}
